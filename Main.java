450882fd6028a7a941377e379a2a8ddded404fad
Artem <aklimichevm@yandex.ru>
2026-02-28 18:42:03 +0300

    lesson_6.1

--- Main.java
+++ Main.java
@@ -1,72 +1,45 @@
-class MyArraySizeException extends Exception {
-    MyArraySizeException(String message) {
-        super(message);
-    }
-}
-
-class MyArrayDataException extends Exception {
-    MyArrayDataException(String message) {
-        super(message);
-    }
-}
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Main {
-    public static boolean checkArray(String[][] array) {
-        if (array == null || array.length != 4) {
-            return false;
-        }
+    static void deleteStudent(HashSet<Student> set) {
+        Iterator<Student> iterator = set.iterator();
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == null || array[i].length != 4) {
-                return false;
+        while (iterator.hasNext()) {
+            Student student = iterator.next();
+
+            if (student.getAverageGrade() < 3) {
+                iterator.remove();
             }
         }
-
-        return true;
     }
 
-    public static int myFunc(String[][] array) throws MyArrayDataException, MyArraySizeException {
-        if (!checkArray(array)) {
-            throw new MyArraySizeException("Массив должен быть 4х4");
-        }
-
-        int sum = 0;
-        for(int i = 0; i < array.length; i++) {
-            for(int j = 0; j < array[i].length; j++) {
-                try {
-                    sum += Integer.parseInt(array[i][j]);
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException(
-                        "Массив содержит некорректные данные\n" + 
-                        "Строка " + (i + 1) + "\n" +
-                        "Столбец " + (j + 1)
-                    );
-                }
+    static void transferStudent(HashSet<Student> set) {
+        for(Student student : set) {
+            if (student.getAverageGrade() >= 3) {
+                student.setCourse(student.getCourse() + 1);
             }
         }
+    }
 
-        return sum;
+    static void printStudents(Set<Student> students, int course) {
+        for(Student student : students) {
+            if (student.getCourse() == course) {
+                System.out.println(student.getName());
+            }
+        }
     }
 
     public static void main(String[] args) {
-        String[][] array = {
-            {"1", "2", "3", "4"},
-            {"1", "2", "3", "4"},
-            {"1", "2", "3", "4"},
-            {"1", "2", "3", "4"},
-        };
-        
-        try {
-            int result = myFunc(array);
-            System.out.print("Результат сложения: " + result);
-        } catch (MyArrayDataException | MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            String value = array[7][0];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Поймали: " + e.getMessage());
-        }
+        HashSet<Student> set = new HashSet<>();
+        set.add(new Student("Иван Петров", "ИС-21", 3, new int[]{5, 5, 4, 5}));
+        set.add(new Student("Мария Соколова", "ПИ-22", 1, new int[]{4, 3, 4, 3}));
+        set.add(new Student("Алексей Новиков", "КБ-20", 2, new int[]{2, 2, 1, 2}));
+        set.add(new Student("Дмитрий Волков", "КБ-20", 2, new int[]{4, 3, 4, 3}));
+    
+        deleteStudent(set);
+        transferStudent(set);
+        printStudents(set, 3);
     }
 }
