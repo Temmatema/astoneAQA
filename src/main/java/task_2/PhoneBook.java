8d1e1eac10297729751b7f84bf486591bccc720f
Artem <aklimichevm@yandex.ru>
2026-03-19 22:59:31 +0300

    исправил инкапсуляцию

--- src/main/java/task_2/PhoneBook.java
+++ src/main/java/task_2/PhoneBook.java
@@ -2,15 +2,23 @@ package task_2;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class PhoneBook {
-    HashMap<String, ArrayList<String>> phoneBook;
+    private final HashMap<String, ArrayList<String>> phoneBook;
 
-    PhoneBook() {
+    public PhoneBook() {
         this.phoneBook = new HashMap<>();
     }
 
     public void add(String lastName, String phone) {
+        for (List<String> phones : phoneBook.values()) {
+            if (phones.contains(phone)) {
+                System.out.println("Номер уже существует: " + phone);
+                return;
+            }
+        }
+
         if (!phoneBook.containsKey(lastName)) {
             phoneBook.put(lastName, new ArrayList<>());
         }
