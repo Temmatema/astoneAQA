6cb7668e5d60732ef1b71f8de6fbd232c797c940
Artem <aklimichevm@yandex.ru>
2026-03-25 18:23:15 +0300

    delete static methods

--- src/main/java/lesson_7_junit/Main.java
+++ src/main/java/lesson_7_junit/Main.java
@@ -1,46 +1,6 @@
 package lesson_7_junit;
 
 public class Main {
-    public static int factorial(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Число должно быть неотрицательным");
-        }
-
-        if (n == 0) {
-            return 1;
-        } 
-        return n * factorial(n - 1);
-    }
-
-    public static double triangleArea(double base, double height) {
-        if (base <= 0 || height <= 0) {
-            throw new IllegalArgumentException("Основание и высота должны быть положительными числами");
-        }
-        return 0.5 * base * height;
-    }
-
-    public static int calculator(int a, int b, String operation) {
-        switch (operation) {
-            case "+":
-                return a + b;
-            case "-":
-                return a - b;
-            case "*":
-                return a * b;
-            case "/":
-                if (b == 0) {
-                    throw new IllegalArgumentException("Деление на ноль невозможно");
-                }
-                return a / b;
-            default:
-                throw new IllegalArgumentException("Недопустимая операция: " + operation);
-        }
-    }
-
-    public static boolean isNumbersEqual(int a, int b) {
-        return a == b;
-    }
-
     public static void main(String[] args) {
     }
 }
--- src/test/java/lesson_7_junit/MainTest.java
+++ src/test/java/lesson_7_junit/MainTest.java
@@ -5,39 +5,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
+    MathUtils mathUtils = new MathUtils();
 
     @Test
     public void testFactorial() {
-        Assertions.assertEquals(1, Main.factorial(0));
-        Assertions.assertEquals(1, Main.factorial(1));
-        Assertions.assertEquals(2, Main.factorial(2));
-        Assertions.assertEquals(6, Main.factorial(3));
-        Assertions.assertEquals(24, Main.factorial(4));
-        Assertions.assertEquals(120, Main.factorial(5));
-        assertThrows(IllegalArgumentException.class, () -> Main.factorial(-1)); 
+        Assertions.assertEquals(1, mathUtils.factorial(0));
+        Assertions.assertEquals(1, mathUtils.factorial(1));
+        Assertions.assertEquals(2, mathUtils.factorial(2));
+        Assertions.assertEquals(6, mathUtils.factorial(3));
+        Assertions.assertEquals(24, mathUtils.factorial(4));
+        Assertions.assertEquals(120, mathUtils.factorial(5));
+        assertThrows(IllegalArgumentException.class, () -> mathUtils.factorial(-1));
     }
 
     @Test
     public void testTriangleArea() {
-        Assertions.assertEquals(10.0, Main.triangleArea(5.0, 4.0), 0.001);
-        Assertions.assertEquals(15.0, Main.triangleArea(6.0, 5.0), 0.001);
-        assertThrows(IllegalArgumentException.class, () -> Main.triangleArea(-1.0, 4.0)); 
-        assertThrows(IllegalArgumentException.class, () -> Main.triangleArea(5.0, -1.0));
+        Assertions.assertEquals(10.0, mathUtils.triangleArea(5.0, 4.0), 0.001);
+        Assertions.assertEquals(15.0, mathUtils.triangleArea(6.0, 5.0), 0.001);
+        assertThrows(IllegalArgumentException.class, () -> mathUtils.triangleArea(-1.0, 4.0));
+        assertThrows(IllegalArgumentException.class, () -> mathUtils.triangleArea(5.0, -1.0));
     }
 
     @Test
     public void testCalculator() {
-        Assertions.assertEquals(5, Main.calculator(2, 3, "+"));
-        Assertions.assertEquals(-1, Main.calculator(2, 3, "-"));
-        Assertions.assertEquals(6, Main.calculator(2, 3, "*"));
-        Assertions.assertEquals(2, Main.calculator(6, 3, "/"));
-        assertThrows(IllegalArgumentException.class, () -> Main.calculator(6, 0, "/")); 
-        assertThrows(IllegalArgumentException.class, () -> Main.calculator(6, 3, "%")); 
+        Assertions.assertEquals(5, mathUtils.calculator(2, 3, "+"));
+        Assertions.assertEquals(-1, mathUtils.calculator(2, 3, "-"));
+        Assertions.assertEquals(6, mathUtils.calculator(2, 3, "*"));
+        Assertions.assertEquals(2, mathUtils.calculator(6, 3, "/"));
+        assertThrows(IllegalArgumentException.class, () -> mathUtils.calculator(6, 0, "/"));
+        assertThrows(IllegalArgumentException.class, () -> mathUtils.calculator(6, 3, "%"));
     }
 
     @Test
     public void testIsInteger() {
-        Assertions.assertTrue(Main.isNumbersEqual(5, 5));
-        Assertions.assertFalse(Main.isNumbersEqual(5, 10));
+        Assertions.assertTrue(mathUtils.isNumbersEqual(5, 5));
+        Assertions.assertFalse(mathUtils.isNumbersEqual(5, 10));
     }
 }
